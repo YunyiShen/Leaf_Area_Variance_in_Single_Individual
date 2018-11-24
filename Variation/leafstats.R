323726b973176028f080d3d3a2f87a64f0fb6de1
YunyiShen <yshen99@outlook.com>
2018-11-23 22:33:04 -0600

    Add Edge Stats

--- Variation/leafstats.R
+++ Variation/leafstats.R
@@ -1,39 +1,75 @@
 leafdata = read.csv("leavesstats.csv")
-boxplot(ECD_mm~Orientation,data = leafdata[leafdata$shade!='Building' ,])
-ECD.lm = lm(ECD_mm~Orientation,data = leafdata[leafdata$shade!='Building',])
-
-ECD.anova = anova(ECD.lm)
+Edge = leafdata$Perimeter_mm/(pi*leafdata$ECD_mm)
+leafdata = data.frame(leafdata,Edge)
 
 data.building = leafdata[leafdata$shade=="Building",]
 data.norm = leafdata[leafdata$shade!="Building",]
+data.building$Orientation = "Building"
+leafdata = rbind(data.norm,data.building) # sepcial treatment to building shade data
 
-data.S = leafdata[data.norm$Orientation=="S",]
-data.N = leafdata[data.norm$Orientation=="N",]
-data.E = leafdata[data.norm$Orientation=="E",]
-data.W = leafdata[data.norm$Orientation=="W",]
+boxplot(ECD_mm~Orientation,data = leafdata)
+boxplot(Edge~Orientation,data = leafdata)
+
+ECD.lm = lm(ECD_mm~Orientation,data = leafdata[leafdata$shade!='Building',])
+ECD.anova = anova(ECD.lm)
+
+Edge.lm = lm(Edge~Orientation,data = leafdata[leafdata$shade!='Building',])
+Edge.anova = anova(Edge.lm)
+
+
+
+data.S = data.norm[data.norm$Orientation=="S",]
+data.N = data.norm[data.norm$Orientation=="N",]
+data.E = data.norm[data.norm$Orientation=="E",]
+data.W = data.norm[data.norm$Orientation=="W",]
 
 n.bootstrap = 1000
 n.sample = 15
-bootstrap.vars = data.frame(matrix(0,nrow = n.bootstrap,ncol = 5))
-colnames(bootstrap.vars)=c("N","E","S","W","building_shade")
+bootstrap.area.vars = data.frame(matrix(0,nrow = n.bootstrap,ncol = 5))
+colnames(bootstrap.area.vars)=c("N","E","S","W","building_shade")
+
+bootstrap.edge.vars = data.frame(matrix(0,nrow = n.bootstrap,ncol = 5))
+colnames(bootstrap.edge.vars)=c("N","E","S","W","building_shade")
+
 
 for(i in 1:n.bootstrap){
   bootstrapsample.N = data.N$ECD_mm[sample.int(nrow(data.N),n.sample)]
-  bootstrap.vars$N[i] = var(bootstrapsample.N)
+  bootstrap.area.vars$N[i] = var(bootstrapsample.N)
   bootstrapsample.E = data.E$ECD_mm[sample.int(nrow(data.E),n.sample)]
-  bootstrap.vars$E[i] = var(bootstrapsample.E)
+  bootstrap.area.vars$E[i] = var(bootstrapsample.E)
   bootstrapsample.S = data.S$ECD_mm[sample.int(nrow(data.S),n.sample)]
-  bootstrap.vars$S[i] = var(bootstrapsample.S)
+  bootstrap.area.vars$S[i] = var(bootstrapsample.S)
   bootstrapsample.W = data.W$ECD_mm[sample.int(nrow(data.W),n.sample)]
-  bootstrap.vars$W[i] = var(bootstrapsample.W)
+  bootstrap.area.vars$W[i] = var(bootstrapsample.W)
   bootstrapsample.building = data.building$ECD_mm[sample.int(nrow(data.building),n.sample)]
-  bootstrap.vars$building_shade[i] = var(bootstrapsample.building)
-}
-boxplot(bootstrap.vars)
+  bootstrap.area.vars$building_shade[i] = var(bootstrapsample.building)
+  
+  
+  bootstrapsample.edge.N = data.N$Edge[sample.int(nrow(data.N),n.sample)]
+  bootstrap.edge.vars$N[i] = var(bootstrapsample.edge.N)
+  bootstrapsample.edge.E = data.E$Edge[sample.int(nrow(data.E),n.sample)]
+  bootstrap.edge.vars$E[i] = var(bootstrapsample.edge.E)
+  bootstrapsample.edge.S = data.S$Edge[sample.int(nrow(data.S),n.sample)]
+  bootstrap.edge.vars$S[i] = var(bootstrapsample.edge.S)
+  bootstrapsample.edge.W = data.W$Edge[sample.int(nrow(data.W),n.sample)]
+  bootstrap.edge.vars$W[i] = var(bootstrapsample.edge.W)
+  bootstrapsample.edge.building = data.building$Edge[sample.int(nrow(data.building),n.sample)]
+  bootstrap.edge.vars$building_shade[i] = var(bootstrapsample.edge.building)
+  
+  }
+boxplot(bootstrap.area.vars)
+boxplot(bootstrap.edge.vars)
 
-boot.data.reshape = data.frame(ECD.var = matrix( as.matrix(bootstrap.vars),nrow = 5*n.bootstrap,ncol = 1)
-                               ,Orientation = rep(colnames(bootstrap.vars),each = n.bootstrap))
+boot.data.reshape = data.frame(ECD.var = matrix( as.matrix(bootstrap.area.vars),nrow = 5*n.bootstrap,ncol = 1)
+                               ,Edge.var = matrix( as.matrix(bootstrap.edge.vars),nrow = 5*n.bootstrap,ncol = 1)
+                               ,Orientation = rep(colnames(bootstrap.area.vars),each = n.bootstrap))
 
-ECD.var.lm = lm(ECD.var~.,data = boot.data.reshape)
-
+ECD.var.lm = lm(ECD.var~Orientation,data = boot.data.reshape)
 anova(ECD.var.lm)
+
+Edge.var.lm = lm(Edge.var~Orientation,data = boot.data.reshape)
+anova(Edge.var.lm)
+
+
+
+
